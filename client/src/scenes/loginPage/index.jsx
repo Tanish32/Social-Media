1e9d31606acf6de7247ecfc49f15424f44be1a6d
Tanish32 <f20210047@hyderabad.bits-pilani.ac.in>
2024-02-25 11:59:47 +0530

    loginPage/Registration page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,4 +1,38 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material"
+import Form from "./Form.jsx"
+
 const LoginPage = () => {
-	return <div>loginPage</div>
+	const theme = useTheme()
+	const isNonMobileScreens = useMediaQuery("(min-width:1000px")
+	return (
+		<Box>
+			{/* Top Bar */}
+			<Box
+				width="100%"
+				backgroundColor={theme.palette.background.alt}
+				p="1rem 6%"
+				textAlign="center">
+				<Typography
+					fontWeight="bold"
+					fontSize="32px" //figure out how this works
+					color="primary">
+					Sociopedia
+				</Typography>
+			</Box>
+			{/* TextDisplay */}
+			<Box
+				width={isNonMobileScreens ? "50%" : "93%"}
+				p="2rem"
+				m="2rem auto"
+				borderRadius="1.5rem"
+				backgroundColor={theme.palette.background.alt}>
+				<Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+					Welcome to Sociopedia, the Social Media for Sociopaths!
+				</Typography>
+				{/* Form */}
+				<Form></Form>
+			</Box>
+		</Box>
+	)
 }
 export default LoginPage

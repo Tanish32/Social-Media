8fa7dee4f41c0af04d6578b76195c58e62587558
Tanish32 <f20210047@hyderabad.bits-pilani.ac.in>
2024-02-27 22:17:44 +0530

    Completed 1

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,66 +3,66 @@ import User from "../models/User.js"
 
 /** CREATE*/
 export const createPost = async (req, res) => {
-  try {
-    const { userId, description, picturePath } = req.body //shouldnt we get the image itself not just the path?
-    const user = await User.findById(userId)
-    const newPost = new Post({
-      userId,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      location: user.location,
-      description,
-      userPicturPath: user.picturePath,
-      picturePath,
-      likes: {},
-      comments: [],
-    })
-    await newPost.save()
-    const post = await Post.find()
-    res.status(201).json(post)
-  } catch (err) {
-    res.status(409).json({ message: err.message })
-  }
+	try {
+		const { userId, description, picturePath } = req.body //shouldnt we get the image itself not just the path?
+		const user = await User.findById(userId)
+		const newPost = new Post({
+			userId,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			location: user.location,
+			description,
+			userPicturPath: user.picturePath,
+			picturePath,
+			likes: {},
+			comments: [],
+		})
+		await newPost.save()
+		const post = await Post.find()
+		res.status(201).json(post) //how is sending the path enough for the front end to render the image?
+	} catch (err) {
+		res.status(409).json({ message: err.message })
+	}
 }
 
 /**READ */
 export const getFeedPosts = async (req, res) => {
-  try {
-    const post = await Post.find()
-    res.status(200).json(post)
-  } catch (err) {
-    res.status(404).json({ message: err.message })
-  }
+	try {
+		const post = await Post.find()
+		res.status(200).json(post)
+	} catch (err) {
+		res.status(404).json({ message: err.message })
+	}
 }
 
 export const getUserPosts = async (req, res) => {
-  try {
-    const { userId } = req.params
-    const post = await Post.find({ userId })
-    res.status(200).json(post)
-  } catch (err) {
-    res.status(404).json({ message: err.message })
-  }
+	try {
+		const { userId } = req.params
+		const post = await Post.find({ userId })
+		res.status(200).json(post)
+	} catch (err) {
+		res.status(404).json({ message: err.message })
+	}
 }
 
 /**UPDATE */
 export const likePosts = async (req, res) => {
-  try {
-    const { id } = req.params // the guy whose post is being liked
-    const { userId } = req.body // the guy who is linking. How do we add attributes to the body in the front end?
-    const post = await Post.findById(id)
-    const isLiked = post.likes.get(userId)
+	try {
+		const { id } = req.params // the guy whose post is being liked
+		const { userId } = req.body // the guy who is linking. How do we add attributes to the body in the front end?
+		const post = await Post.findById(id)
+		const isLiked = post.likes.get(userId)
 
-    if (isLiked) post.likes.delete(userId)
-    else post.likes.set(userId, true)
+		if (isLiked) post.likes.delete(userId)
+		else post.likes.set(userId, true)
 
-    const updatedPost = await Post.findByIdAndUpdate(
-      id,
-      { likes: post.likes },
-      { new: true }
-    )
-    res.status(200).json(updatedPost)
-  } catch (err) {
-    res.status(404).json({ message: err.message })
-  }
+		const updatedPost = await Post.findByIdAndUpdate(
+			id,
+			{ likes: post.likes },
+			{ new: true }
+		)
+		res.status(200).json(updatedPost)
+	} catch (err) {
+		res.status(404).json({ message: err.message })
+	}
 }
